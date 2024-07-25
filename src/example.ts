ac0e57fc7ac3e51df224b10e999a35561d72473f
王永 <>
2024-07-25 20:11:06 +0800

    format code

---
+++ src/example.ts
@@ -0,0 +1,14 @@
+type User = {
+  name: string;
+  age: number;
+};
+function isAdult(user: User): boolean {
+  return user.age >= 18;
+}
+const justine: User = {
+  name: 'Justine',
+  age: 3,
+};
+const isJustineAnAdult: boolean = isAdult(justine);
+
+console.log('isJustineAnAdult is ', isJustineAnAdult);
