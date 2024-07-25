deae7f2d32c52abf01fbbe2dad9f75de281c23fc
王永 <>
2024-07-25 20:49:38 +0800

    增加数据库连接

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: '116.62.212.156',
+      port: 3306,
+      username: 'root',
+      password: 'fzx416111',
+      database: 'suzhong_copy',
+      entities: [],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/demo/server.ts
+++ src/demo/server.ts
@@ -6,7 +6,7 @@ const port = 3000;
 const server = createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World1111');
+  res.end('Hello World');
 });
 
 server.listen(port, hostname, () => {
