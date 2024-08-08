572b92d586f4412419062f3a10edf4d43f6b9f57
王永 <>
2024-08-08 18:15:05 +0800

    算法

---
+++ src/demo/test.ts
@@ -0,0 +1,36 @@
+function threeSum(nums) {
+  nums.sort((a, b) => a - b); // 先对数组进行排序
+  const result = [];
+
+  for (let i = 0; i < nums.length - 2; i++) {
+    // 跳过重复的元素
+    if (i > 0 && nums[i] === nums[i - 1]) continue;
+
+    let left = i + 1;
+    let right = nums.length - 1;
+
+    while (left < right) {
+      const sum = nums[i] + nums[left] + nums[right];
+
+      if (sum === 0) {
+        result.push([nums[i], nums[left], nums[right]]);
+        // 跳过重复的元素
+        while (left < right && nums[left] === nums[left + 1]) left++;
+        while (left < right && nums[right] === nums[right - 1]) right--;
+        left++;
+        right--;
+      } else if (sum < 0) {
+        left++;
+      } else {
+        right--;
+      }
+    }
+  }
+
+  return result;
+}
+
+// 示例用法
+const nums = [-1, 0, 1, 2, -1, -4];
+
+console.log(threeSum(nums));
